798960f2a18ae3db0d79cbc5b8c49e959de31ea0
Yuri Dias Pereira <yuristardias@hotmail.com>
2024-05-19 00:20:14 -0300

    Correção de bugs

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,5 @@
-
-async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/yuridiasp/js-developer-portfolio/main/data/profile.json';
-    const response = await fetch(url)
-    const profileData = await response.json()
-    return profileData
+async function loadProfileData() {
+    const url = "https://raw.githubusercontent.com/yuridiasp/js-developer-portfolio/main/data/profile.json"
+    
+    return (await fetch(url)).json()
 }
